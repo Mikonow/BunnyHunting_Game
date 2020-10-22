5f57d1c78981af5f3a3cd76a74592385fa813c14
Mikonow <verstraeten.noemie@outlook.fr>
2020-10-22 17:35:15 +0200

    Premier commit
    Premier commit : jeu avec les scripts de base.

---
+++ Assets/RabbitDisparition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitDisparition : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        print("TestEnter");
+
+        if (Input.GetKey(KeyCode.E)) 
+        {
+            print("TestDestroy");
+            Destroy(gameObject); 
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+}
---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    //prefab du lapin
+    public GameObject lapin;
+
+    //groupe contenant les lapins
+    public GameObject groupelapin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tableau qui liste les composants des enfants de SpawnLocations
+        Transform[] allChildren = GetComponentsInChildren<Transform>();
+
+        //on converti notre tableau en liste
+        List<Transform> children = new List<Transform>(allChildren);
+
+        //On enlève le transform de SpawnLocations si non on aura un lapin dans le vide lol
+        children.RemoveAt(0);
+
+        foreach(Transform child in children)
+        {
+            //on met les lapins dans le groupe ChocolateRabbits pour qu'ils scalent à la bonne taille
+            GameObject currentlapin = Instantiate(lapin, groupelapin.transform);
+
+            //on assigne les lapins aux différentes positions situées dans les transform des child de SpawnLocations
+            currentlapin.transform.position = new Vector3(child.position.x, child.position.y, child.position.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
